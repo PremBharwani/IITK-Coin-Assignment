fcd5213ae719a05aa47a9d2be303e39e11e8d17c
dirtbkr <dirtbkr7736@gmail.com>
2021-05-17 13:53:34 +0530

    Added comments and some beautification

--- main.py
+++ main.py
@@ -4,34 +4,27 @@ from flask.wrappers import Request
 
 app =  Flask(__name__)
 
+#Below is the dummy data
 dummy_data={ '1':469 , '2':200, '3':461 , '4':123 , '200269':540}
 
+#Defining the /coins endpoint
 @app.route('/coins', methods=['POST'])
 def coins_method():
+
+    # Extracting the json data from the http request 
     input_json = request.get_json(force=True)
     rollno = input_json["rollno"]
+
+    # Checking if the rollno exists in records
     if rollno in dummy_data:
-        app.logger.info("Found {} coins balace ".format(rollno))
+        #If the rollno is found in dummy data we return the number of coins as JSON
+        app.logger.info("\n*******\nRoll number {} is found in the dummy data\n*******\n".format(rollno))
         dictToReturnIfFound = {'coins':dummy_data[rollno]}
         return jsonify(dictToReturnIfFound)
+    #If the rollno was not found , we will output coins value to be null
+    app.logger.info("\n*******\nRoll number {} NOT found in the dummy data\n*******\n".format(rollno))
     dictToReturn = {'coins':'null'}
     return jsonify(dictToReturn)
-    # coins_dict={ '1':469 , '2':200, '3':461 , '4':123 }
-    # rollno=str(request.form.get("rollno"))
-    # app.logger.info("Roll Number : "+rollno)
-    # app.logger.info("req.form.items : "+str(request.form.items))
-    # prem_dic=request.form
-    # prem_json = request.get_json
-    # prem_data = request.get_data
-    # app.logger.info("Get json : "+str(prem_json))
-    # app.logger.info("Get Data : "+str(prem_data))
-    
-    # # app.logger.info("Request form "+str(request.form))
-    # return prem_dic
-    
-
-
-    
     
 
 if __name__=="__main__":
